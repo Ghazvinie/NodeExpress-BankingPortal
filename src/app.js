9b5d11b820e5d6904d9f216cb6d305f35a2ae5e9
Daniel Ghazvinie <danielghazvinie@gmail.com>
2021-05-15 15:06:51 +0100

    module 3

--- src/app.js
+++ src/app.js
@@ -39,6 +39,9 @@ app.get('/profile', (req, res) => {
 });
 
 // Root route
-app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Account Summary', accounts: accounts });
+});
+
 
 app.listen(3000, () => console.log('PS Project Running on Port 3000!'));
