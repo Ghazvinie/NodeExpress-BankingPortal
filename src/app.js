d3ed9a61db047f193908669b2ceb4f5a6b55b860
Daniel Ghazvinie <danielghazvinie@gmail.com>
2021-05-15 13:23:12 +0100

    module 2

--- src/app.js
+++ src/app.js
@@ -4,5 +4,8 @@ const express = require('express');
 
 const app = express();
 
+// Set view engine
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+
+app.set('static', path.join(__dirname, 'public'));
