1ec3f6b2819fb1eb19ec0432fe544f131e126a55
Daniel Ghazvinie <danielghazvinie@gmail.com>
2021-05-15 16:29:22 +0100

    module 5

--- src/data.js
+++ src/data.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-const writeJSON = () => {
 // Read account data
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
@@ -9,6 +8,10 @@ const accounts = JSON.parse(accountData);
 // Read user data
 const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), "utf8");
 const users = JSON.parse(userData);
+
+const writeJSON = () => {
+    let accountsJSON = JSON.stringify(accounts, null, 4)
+    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
 };
 
 module.exports = { accounts, users, writeJSON };
