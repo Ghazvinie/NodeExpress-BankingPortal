2a5a2ac88b0022c1b21f73c157fe3c7ada1d3ac4
Daniel Ghazvinie <danielghazvinie@gmail.com>
2021-05-15 15:01:38 +0100

    module 3

--- src/app.js
+++ src/app.js
@@ -20,9 +20,29 @@ const accounts = JSON.parse(accountData);
 const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), "utf8");
 const users = JSON.parse(userData);
 
+// Account routes
+app.get('/savings', (req, res) => {
+    res.render('account', { account: accounts.savings});
+});
+
+app.get('/checking', (req, res) => {
+    res.render('checking', { account: accounts.checking});
+});
+
+app.get('/credit', (req, res) => {
+    res.render('credit', { account: accounts.credit});
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile', { user: users[0]});
+});
+
 // Root route
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Index'});
+    res.render('index', {
+        title: 'Accounts Summary',
+        account : accounts
+    });
 
 });
 
