e9101a4f0bf4aad23effec183be15958e6ac3f59
Daniel Ghazvinie <danielghazvinie@gmail.com>
2021-05-15 13:44:36 +0100

    module 2

--- src/app.js
+++ src/app.js
@@ -4,9 +4,16 @@ const express = require('express');
 
 const app = express();
 
-// Set view engine
+// Set view engine (don't need to use path.join())
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Set static directory (don't need to use path.join())
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+});
+
+app.listen(8080);
