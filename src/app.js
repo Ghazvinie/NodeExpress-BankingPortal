2642cc2f651bb242529f6da9bab57ce94e7e6cef
Daniel Ghazvinie <danielghazvinie@gmail.com>
2021-05-15 14:40:05 +0100

    module 3

--- src/app.js
+++ src/app.js
@@ -12,11 +12,14 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+// Read account data
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+// Read user data
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), "utf8");
 const users = JSON.parse(userData);
+
 // Root route
 app.get('/', (req, res) => {
     res.render('index', {title: 'Index'});
